2039ff59af95f03dd2d32f466731bc6e9185ee8e
sravani <sandhyapyla.8@gmail.com>
2024-07-05 18:18:52 +0530

    using sheetdb create new spreadsheet and it is saving the data when i enter question it is storing the sheet

--- src/openApiAi.js
+++ src/openApiAi.js
@@ -1,6 +1,7 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const API_KEY=process.env.REACT_APP_GEMINI_API_KEY
-// console.log("apiKey",API_KEY)
+const GOOGLE_SHEET=process.env.REACT_APP_GOOGLE_SHEET
+// console.log("apiKey",GOOGLE_SHEET)
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 const model = genAI.getGenerativeModel({
@@ -12,8 +13,25 @@ export async function run(prompt) {
     const response = await result.response;
     const text = response.text();
     console.log(text);
-    return text;
+    const data={
+      Timestamp:Date.now(),
+      Prompt:prompt,
+      Post:text,
+    }
+    const newResponse=await fetchData(data);
+    return newResponse;
 }
+
+async function fetchData(data) {
+    await fetch(GOOGLE_SHEET,{
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({data})
+    });
+}
+
 
 
 
